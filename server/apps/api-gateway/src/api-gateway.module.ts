1246f6bae0a2d4b1f08f7de27f1cd51a48070962
Shanelka Pramuditha <99pramuditha@gmail.com>
2025-03-23 23:57:49 +0530

    enhance API gateway setup with environment configuration and logging

--- server/apps/api-gateway/src/api-gateway.module.ts
+++ server/apps/api-gateway/src/api-gateway.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+      expandVariables: true,
+      load: [],
+    }),
+  ],
   controllers: [ApiGatewayController],
   providers: [ApiGatewayService],
 })
--- server/apps/api-gateway/src/main.ts
+++ server/apps/api-gateway/src/main.ts
@@ -3,6 +3,9 @@ import { ApiGatewayModule } from './api-gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
+  // Console log the server URL
+  const serverUrl = await app.getUrl();
+  console.log(`API Gateway is running at: ${serverUrl}`);
 }
-bootstrap();
+bootstrap().catch((err) => console.error('Bootstrap failed:', err));
