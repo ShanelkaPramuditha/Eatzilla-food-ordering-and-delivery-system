949a32dac001765af351cf95d84ebfbd5064fa8b
Shanelka Pramuditha <99pramuditha@gmail.com>
2025-03-23 15:15:09 +0530

    add initial server setup with API gateway, Docker support, and Prettier configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
 import '@/styles/globals.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from './components/ui/button';
 
 const App = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  const handleLogin = () => {
+    loginWithRedirect();
+  };
+  const handleLogout = () => {
+    logout({ logoutParams: { returnTo: window.location.origin } });
+  };
+
   return (
     <div>
       <h1>Rsbuild with React</h1>
+      {isAuthenticated ? (
+        <Button onClick={handleLogout}>Logout</Button>
+      ) : (
+        <Button onClick={handleLogin}>Login</Button>
+      )}
     </div>
   );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 const rootEl = document.getElementById('root');
@@ -7,7 +8,15 @@ if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <Auth0Provider
+        domain={import.meta.env.PUBLIC_AUTH0_DOMAIN}
+        clientId={import.meta.env.PUBLIC_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
     </React.StrictMode>,
   );
 }
