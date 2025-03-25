b371b28aa3e82a52f6391d63613db0748af50eba
Shanelka Pramuditha <99pramuditha@gmail.com>
2025-03-25 13:21:38 +0530

    enhance authentication flow with loading state and user details; refactor login/logout buttons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,55 @@
 import '@/styles/globals.css';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from './components/ui/button';
+import { Loader } from 'lucide-react';
 
 const App = () => {
-  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
-  const handleLogin = () => {
-    loginWithRedirect();
-  };
-  const handleLogout = () => {
-    logout({ logoutParams: { returnTo: window.location.origin } });
-  };
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <div className='flex w-screen h-screen items-center justify-center'>
+        <Loader className='animate-spin' />
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <h1>Rsbuild with React</h1>
-      {isAuthenticated ? (
-        <Button onClick={handleLogout}>Logout</Button>
-      ) : (
-        <Button onClick={handleLogin}>Login</Button>
-      )}
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <div className='mt-4'>
+        {isAuthenticated && !isLoading ? (
+          <>
+            <div>
+              <img src={user?.picture} alt={user?.name} />
+              <h2>User Name: {user?.name}</h2>
+              <p>Email: {user?.email}</p>
+            </div>
+            <LogoutButton />
+          </>
+        ) : (
+          <LoginButton />
+        )}
+      </div>
     </div>
   );
 };
 
 export default App;
+
+// Login and Logout buttons
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
+};
+
+// Logout button
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <Button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+    </Button>
+  );
+};
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,16 +4,21 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 const rootEl = document.getElementById('root');
+
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
+
   root.render(
     <React.StrictMode>
       <Auth0Provider
         domain={import.meta.env.PUBLIC_AUTH0_DOMAIN}
         clientId={import.meta.env.PUBLIC_AUTH0_CLIENT_ID}
         authorizationParams={{
+          audience: import.meta.env.PUBLIC_AUTH0_AUDIENCE,
           redirect_uri: window.location.origin,
         }}
+        cacheLocation='localstorage'
+        useRefreshTokens={true}
       >
         <App />
       </Auth0Provider>
