d89fd8a991e2c76743c9c92cbc78a8cd8fcd3ab6
eshannayanapriya <eshannayanapriyaprofessional@gmail.com>
2025-04-24 14:37:12 +0530

    feat(sidebar): add 'Delivery' item to delivery person sidebar

--- client/src/configs/sidebar-config/items.ts
+++ client/src/configs/sidebar-config/items.ts
@@ -29,4 +29,9 @@ export const DeliveryPersonSidebarItems = [
     url: '/orders',
     icon: IconTruckDelivery,
   },
+  {
+    title: 'Delivery',
+    url: '/delivery',
+    icon: IconTruckDelivery,
+  }
 ];
