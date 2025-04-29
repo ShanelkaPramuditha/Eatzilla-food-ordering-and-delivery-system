1b452e565e4cc977de93cfe1f9fe4b73e1faac6d
Shanelka Pramuditha <99pramuditha@gmail.com>
2025-04-29 17:59:18 +0530

    style: update HomePage layout for improved responsiveness

--- client/src/routes/_home/-food/index.tsx
+++ client/src/routes/_home/-food/index.tsx
@@ -2,7 +2,7 @@ import FoodMenu from '@/components/food/food-menu';
 
 export function HomePage() {
   return (
-    <div>
+    <div className='flex w-full flex-col items-center justify-center'>
       <FoodMenu />
     </div>
   );
